fa1c2212f25e7e4ffe6628e488ba2c478f97073c
Mark Woods <mwoods@online.ie>
2015-04-07 08:12:19 +0100

    Better names and namespacing for devise helpers

--- lib/cucumber/futuregov/steps/devise.rb
+++ lib/cucumber/futuregov/steps/devise.rb
@@ -1,47 +1,62 @@
-# include warden test helpers to allow fast sign in for given steps
+# use warden test mode to allow fast sign in for given steps
 Warden.test_mode!
-World(Warden::Test::Helpers)
 After { Warden.test_reset! }
 
-module FGDeviseHelpers
-  def sign_in_as(user, attrs={})
-    email = attrs[:email] || user.email
-    password = attrs[:password] || user.password
-    scope = Devise::Mapping.find_scope!(user)
-    visit send("new_#{scope}_session_path")
-    within(:xpath, "//form[contains(@action,'sign_in')]") do
-      fill_in "#{scope}_email", with: email
-      fill_in "#{scope}_password", with: password
-      find(:css, "input[type='submit']").click
+module Cucumber
+  module FutureGov
+    module DeviseHelper
+
+      include Warden::Test::Helpers
+
+      def sign_in_as(user, attrs={})
+        email = attrs[:email] || user.email
+        password = attrs[:password] || user.password
+        scope = Devise::Mapping.find_scope!(user)
+        visit send("new_#{scope}_session_path")
+        within(:xpath, "//form[contains(@action,'sign_in')]") do
+          fill_in "#{scope}_email", with: email
+          fill_in "#{scope}_password", with: password
+          find(:css, "input[type='submit']").click
+        end
+      end
+
+      def given_sign_in_as(user)
+        # Fast sign in for given steps, bypass devise and use warden test
+        # helper to create authenticated session, without driving the ui.
+        # See https://github.com/hassox/warden/wiki/Testing
+        login_as(user)
+      end
+
+      def sign_out_as(user)
+        sign_out
+      end
+
+      def sign_out
+        node = find(:xpath, "//form[contains(@action,'sign_out')]")
+      rescue Capybara::Ambiguous
+        # FIXME: this sucks, at least make the warning clearer, also look at
+        # removing entirely or requiring explicit acceptance of first match
+        warn "WARNING: #{$!.message}"
+        node = first(:xpath, "//form[contains(@action,'sign_out')]")
+      ensure
+        within(node) do
+          find(:css, "input[type='submit']").click
+        end
+      end
+
+      def should_be_signed_in_as(user)
+        should_see_sign_out
+      end
+
+      def should_see_sign_out
+        should have_xpath("//form[contains(@action,'sign_out')]")
+      end
+
     end
-  end
-
-  def given_sign_in_as(user)
-    # Fast sign in for given steps, bypass devise and use warden test
-    # helper to create authenticated session, without driving the ui.
-    # See https://github.com/hassox/warden/wiki/Testing
-    login_as(user)
-  end
-
-  def sign_out
-    node = find(:xpath, "//form[contains(@action,'sign_out')]")
-  rescue Capybara::Ambiguous
-    # FIXME: this sucks, at least make the warning clearer, also look at
-    # removing entirely or requiring explicit acceptance of first match
-    warn "WARNING: #{$!.message}"
-    node = first(:xpath, "//form[contains(@action,'sign_out')]")
-  ensure
-    within(node) do
-      find(:css, "input[type='submit']").click
-    end
-  end
-
-  def should_see_sign_out
-    should have_xpath("//form[contains(@action,'sign_out')]")
   end
 end
 
-World(FGDeviseHelpers)
+World(Cucumber::FutureGov::DeviseHelper)
 
 Given(/^I have signed in$/) do
   given_sign_in_as @i
@@ -52,9 +67,9 @@ When(/^I sign in$/) do
 end
 
 When(/^I sign out$/) do
-  sign_out
+  sign_out_as @i
 end
 
 Then(/^I should be signed in$/) do
-  should_see_sign_out
+  should_be_signed_in_as @i
 end
